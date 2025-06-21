9b398fda0cb3d27fc6d4e11b1c4c8c53d0f49f3b
Richard Kang <2356020+kangks@users.noreply.github.com>
2025-06-21 15:30:08 +0800

    aiortc with Nova

--- simple-pipecat-webrtc/api/app_simple.py
+++ simple-pipecat-webrtc/api/app_simple.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 
 # Configure logging
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 logger = logging.getLogger("webrtc-api")
